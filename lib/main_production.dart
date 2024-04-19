0e0d17143590f5836ee69950b8dd946c7ea2b0b5
Samuel Mothwa <samuel.mothwa@shoprite.co.za>
2024-04-19 21:11:54 +0200

    add, view and remove passwords

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,15 +1,14 @@
 import 'package:cyberman/app/app.dart';
 import 'package:cyberman/bootstrap.dart';
-import 'package:cyberman/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  await openDatabase(
+    join(await getDatabasesPath(), 'cyberman.db'),
   );
   await bootstrap(App.new);
 }
--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,15 +1,14 @@
 import 'package:cyberman/app/app.dart';
 import 'package:cyberman/bootstrap.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  final database = openDatabase(
+    join(await getDatabasesPath(), 'cyberman.stg.db'),
   );
-  bootstrap(App.new);
+  await bootstrap(App.new);
 }
